8b42c1ab984075d684a985d3325d69d3225a5ce9
jaywomack <womackjw@hotmail.com>
2021-06-07 22:14:03 -0700

    Finished the 18 module

--- section_3/notes-app/app.js
+++ section_3/notes-app/app.js
@@ -6,5 +6,54 @@ const yargs = require('yargs');
 
 //My modules
 const getNotes = require('./notes');
+yargs.version('1.1.0');
 
-console.log(yargs.argv);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title + ',', 'Body: ' + argv.body);
+  }
+});
+
+// Create a remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing a note!');
+  }
+});
+
+// Create a read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading a note');
+  }
+});
+
+// Create a list command
+yargs.command({
+  command: 'list',
+  describe: 'List all commands',
+  handler: function () {
+    console.log('Listing all commands.');
+  }
+});
+// add, remove, read, list
+yargs.parse();
