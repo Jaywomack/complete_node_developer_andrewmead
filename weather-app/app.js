3ef0d42bdbd3570198b5d06319cb189c910a77c3
jaywomack <womackjw@hotmail.com>
2021-06-09 18:55:31 -0700

    finished http reguests and handling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,27 @@
 const axios = require('axios');
 require('dotenv').config();
-
 axios
   .get(
-    `http://api.weatherstack.com/current?access_key=${process.env.SECRET_KEY}&query=New York`
+    `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_SECRET_KEY}&query=New York&units=f`
   )
   .then((response) => {
-    console.log(response);
+    const data = response.data;
+    console.log(response.data);
   })
   .catch((error) => {
     console.log(error.message);
   });
+
+// axios
+//   .get(
+//     `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_SECRET_KEY}&limit=1`
+//   )
+//   .then((response) => {
+//     const data = response.data;
+//     const latitude = data.features[0].geometry.coordinates[0];
+//     const longitude = data.features[0].geometry.coordinates[1];
+//     console.log(latitude, longitude);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
